2be49a1a56fcdb94aa99b1fdf171835fc713bc41
ronal <ronaldr.vq@gmail.com>
2021-06-07 15:47:02 -0500

    Initial commit

---
+++ app/src/pro/java/com.example.fravorsversionesdecompilacion/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.fravorsversionesdecompilacion;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void probarVersion(View vista){
+        Toast.makeText(this, "esta version es PAGA", Toast.LENGTH_SHORT).show();
+    }
+
+    // esto e spara verificar en los activitis comunes que version de app es
+/*    if (BuildConfig.FLAVOR.equals("admin")) {
+    ...
+    }*/
+
+
+}
